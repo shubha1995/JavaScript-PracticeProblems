afcaa20e49f07f2cd113856286255c90e387e190
shubhasankar <ssbhaumikdmr@gmail.com>
2021-08-11 00:07:38 +0530

    Added: Harmonic Number

---
+++ HarmonicNumber.js
@@ -0,0 +1,9 @@
+let number = Math.floor(Math.random() * 10);
+console.log("Random Number to find Harmonic number: " + number);
+let H = 0;
+for (i = 1; i <= number; i++) {
+    H = 1 / i + H;
+    console.log("Harmonic number of position " + (
+        i
+    ) + " is " + H);
+}
